3ebdbb3c0cb2d707e0eff00898afb9b70154e4f5
ETH1Elohim <ezmax.defi@gmail.com>
2023-01-10 16:05:54 -0700

    add helper config

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -3,11 +3,21 @@
 // hre.getNamedAccounts
 // hre.deployments
 
+const { networkConfig } = require("../helper-hardhat-config")
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
+    // if chainId is X use address Y
+    // if chainId is Z use address A
+
     // when going for localhost or hardhat network we want to use a mock
     // https://docs.chain.link/docs/ethereum-addresses for different chains - insert in PriceConverter.sol priceFeed
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [address], // price feed address
+        log: true,
+    })
 }
